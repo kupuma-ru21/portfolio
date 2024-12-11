c99707e2d5318da732a388ac34faf9c3aaf10d45
kupuma-ru21 <tech.kupumaru@gmail.com>
2024-12-10 23:01:13 -0800

    feat: add google tag manager

--- client/@types/vite.env.d.ts
+++ client/@types/vite.env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_GRAPHQL_SCHEMA_URL: string;
   readonly VITE_GOOGLE_ANALYTICS_MEASUREMENT_ID: string;
+  readonly VITE_GOOGLE_TAG_MANAGER_CONTAINER_ID: string;
 }
 
 interface ImportMeta {
--- client/app/root.tsx
+++ client/app/root.tsx
@@ -121,6 +121,19 @@ const Document = ({ children, locale, dir }: DocumentProps) => {
         <Meta />
         <Links />
         <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${
+              import.meta.env.VITE_GOOGLE_TAG_MANAGER_CONTAINER_ID
+            }');
+        `,
+          }}
+        />
+        <script
           async
           src={`https://www.googletagmanager.com/gtag/js?id=${
             import.meta.env.VITE_GOOGLE_ANALYTICS_MEASUREMENT_ID
@@ -140,6 +153,16 @@ const Document = ({ children, locale, dir }: DocumentProps) => {
         />
       </head>
       <body>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${
+              import.meta.env.VITE_GOOGLE_TAG_MANAGER_CONTAINER_ID
+            }`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         {children}
         <ScrollRestoration />
         <Scripts />
