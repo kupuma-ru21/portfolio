4208e42cc954b06b9c7cd2cbc30f01b0c493e283
Koichi Kimura <tech.kupumaru@gmail.com>
2024-12-14 13:36:40 -0800

    feat: add env variables for contact (#16)

--- client/@types/vite.env.d.ts
+++ client/@types/vite.env.d.ts
@@ -4,6 +4,8 @@ interface ImportMetaEnv {
   readonly VITE_GRAPHQL_SCHEMA_URL: string;
   readonly VITE_GOOGLE_ANALYTICS_MEASUREMENT_ID: string;
   readonly VITE_GOOGLE_TAG_MANAGER_CONTAINER_ID: string;
+  readonly VITE_EMAIL: string;
+  readonly VITE_RESEND_API_KEY: string;
 }
 
 interface ImportMeta {
