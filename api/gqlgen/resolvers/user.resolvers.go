73e35fcaf989628acd7802cbc6d84242be1ae61e
kupuma-ru21 <tech.kupumaru@gmail.com>
2024-12-14 11:28:09 -0800

    chore: remove TODO

--- api/gqlgen/resolvers/user.resolvers.go
+++ api/gqlgen/resolvers/user.resolvers.go
@@ -36,13 +36,6 @@ func (r *mutationResolver) CreateUser(ctx context.Context, input ent.CreateUserI
 		return uuid.Nil, err
 	}
 
-	// TODO: use this code for login function
-	// password1 := []byte(string(passwordHashed))
-	// err = bcrypt.CompareHashAndPassword(password1, []byte(input.Password))
-	// if err != nil {
-	// 	return uuid.Nil, errCustom.Create("Invalid password")
-	// }
-
 	user, err := r.Client.User.Create().SetEmail(input.Email).SetPassword(string(passwordHashed)).Save(ctx)
 	if err != nil {
 		return uuid.Nil, err
