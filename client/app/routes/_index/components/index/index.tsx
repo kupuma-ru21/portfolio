058972c4aa5d7d1d90e10a26f3a920b1e928715f
kupuma-ru21 <tech.kupumaru@gmail.com>
2024-12-16 16:08:39 -0800

    fix: show apps from api instead of client on top page

--- client/app/routes/_index/components/index/index.tsx
+++ client/app/routes/_index/components/index/index.tsx
@@ -1,63 +1,69 @@
 import { Box, Divider, Flex, Heading, Image, Text } from "@chakra-ui/react";
-// import { type AppsQuery } from "gql/graphql";
+import { AppLinkType, type AppFragment } from "gql/graphql";
 import { CardContent } from "../card-body";
 import { useIndex } from "./useIndex";
 import { Card } from "~/components/card";
 
-export const Index = () =>
-  // { apps }: { apps: AppsQuery["apps"] }
-  {
-    const { t, appData } = useIndex();
+export const Index = ({
+  apps,
+}: {
+  apps: (AppFragment & { link: string; linkType: AppLinkType })[];
+}) => {
+  const { t } = useIndex();
 
-    return (
-      <Box py="20px">
-        <Flex
-          w="fit-content"
-          m="auto"
-          gap="16px"
-          mb="48px"
-          direction={{ base: "column", md: "initial" }}
-          alignItems={{ base: "center", md: "initial" }}
-        >
-          <Heading as="h1" alignContent="center">
-            <Text as="span" fontWeight={400}>
-              {t("Hi! I'm")}{" "}
-            </Text>
-            <Text as="strong">{t("Koichi Kimura,")}</Text>
-            <Text as="span" fontWeight={400} display="block">
-              {t("a Frontend developer")} 👋
-            </Text>
-          </Heading>
-          <Image
-            src="/images/index/profile.avif"
-            alt="Koichi Kimura"
-            w="247px"
-            h="283px"
-            borderRadius="8px"
-            pointerEvents="none"
-          />
+  return (
+    <Box py="20px">
+      <Flex
+        w="fit-content"
+        m="auto"
+        gap="16px"
+        mb="48px"
+        direction={{ base: "column", md: "initial" }}
+        alignItems={{ base: "center", md: "initial" }}
+      >
+        <Heading as="h1" alignContent="center">
+          <Text as="span" fontWeight={400}>
+            {t("Hi! I'm")}{" "}
+          </Text>
+          <Text as="strong">{t("Koichi Kimura,")}</Text>
+          <Text as="span" fontWeight={400} display="block">
+            {t("a Frontend developer")} 👋
+          </Text>
+        </Heading>
+        <Image
+          src="/images/index/profile.avif"
+          alt="Koichi Kimura"
+          w="247px"
+          h="283px"
+          borderRadius="8px"
+          pointerEvents="none"
+        />
+      </Flex>
+      <Divider />
+      <Box p="16px">
+        <Heading mb="16px" textAlign="center" fontWeight={600}>
+          {t("Applications I developed at work")}
+        </Heading>
+        <Flex direction="column" gap="16px">
+          {apps.map(({ id, imageURL, title, detail, link, linkType }) => {
+            return (
+              <Card key={id}>
+                <Card.Image src={imageURL} alt={title} />
+                <CardContent
+                  title={title}
+                  detail={detail}
+                  href={link}
+                  linkText={
+                    linkType === AppLinkType.App
+                      ? t("Move to the site")
+                      : t("Move to company site")
+                  }
+                />
+              </Card>
+            );
+          })}
         </Flex>
-        <Divider />
-        <Box p="16px">
-          <Heading mb="16px" textAlign="center" fontWeight={600}>
-            {t("Applications I developed at work")}
-          </Heading>
-          <Flex direction="column" gap="16px">
-            {appData.map(({ src, title, description, href, linkText }) => {
-              return (
-                <Card key={title}>
-                  <Card.Image src={src} alt={title} />
-                  <CardContent
-                    title={title}
-                    detail={description}
-                    href={href}
-                    linkText={linkText}
-                  />
-                </Card>
-              );
-            })}
-          </Flex>
-        </Box>
       </Box>
-    );
-  };
+    </Box>
+  );
+};
--- client/app/routes/_index/components/index/useIndex.ts
+++ client/app/routes/_index/components/index/useIndex.ts
@@ -1,52 +1,7 @@
-import { useRouteLoaderData } from "@remix-run/react";
 import { useTranslation } from "react-i18next";
-import { getPathByLocale } from "./utils";
-import { LOCALES } from "~/constants";
-import { type loader } from "~/root";
 
 export const useIndex = () => {
   const { t } = useTranslation("index");
 
-  const routeLoaderData = useRouteLoaderData<typeof loader>("root");
-  const locale = routeLoaderData?.locale ?? LOCALES.en;
-
-  const appData = [
-    {
-      src: "/images/index/sorajima.avif",
-      title: "Sorajima Toon",
-      description: t("descriptionAboutSorajimaToon"),
-      href: "https://sorajimatoon.com/",
-      linkText: t("Move to the site"),
-    },
-    {
-      src: "/images/index/buysell.avif",
-      title: "BuySell",
-      description: t("descriptionAboutBuySellApp"),
-      href: `https://buysell-technologies.com/${getPathByLocale(locale)}`,
-      linkText: t("Move to company site"),
-    },
-    {
-      src: "/images/index/visits-forms.avif",
-      title: "Visits Forms",
-      description: t("descriptionAboutVisitsForms"),
-      href: "https://visitsforms.com/",
-      linkText: t("Move to company site"),
-    },
-    {
-      src: "/images/index/sony-bank.avif",
-      title: "Sony bank",
-      description: t("descriptionAboutSonyBank"),
-      href: `https://moneykit.net/${getPathByLocale(locale)}`,
-      linkText: t("Move to company site"),
-    },
-    {
-      src: "/images/index/softbank.avif",
-      title: "Logistics app",
-      description: t("descriptionAboutSbLogistics"),
-      href: "https://www.sbfw.co.jp/",
-      linkText: t("Move to company site"),
-    },
-  ];
-
-  return { t, appData };
+  return { t };
 };
