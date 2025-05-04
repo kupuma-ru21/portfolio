75464264d3bdd530a132d7a4d15d7b1bda982d4a
kupuma-ru21 <tech.kupumaru@gmail.com>
2025-05-04 20:35:11 +0900

    fix: add copy-env to make dev command work

--- client/@types/resources.d.ts
+++ client/@types/resources.d.ts
@@ -1,118 +1,118 @@
 interface Resources {
-  about: {
-    About: "About";
-    description: "Let me introduce myself!";
-    "I like to do": "I like to do...";
+  "about": {
+    "About": "About",
+    "description": "Let me introduce myself!",
+    "I like to do": "I like to do...",
     "software development": {
-      title: "Software Development";
-      description: "I'm especially interested in how to improve maintainability and performance. For example, I've used just avif images in this project cuz it's better in performance than jpg, png and things like that.";
-    };
+      "title": "Software Development",
+      "description": "I'm especially interested in how to improve maintainability and performance. For example, I've used just avif images in this project cuz it's better in performance than jpg, png and things like that."
+    },
     "reading comics and watching anime": {
-      title: "Reading comics and watching anime";
-      description1: "I'm a big fan of anime and comics. I've read countless comics and anime.";
-      description2: "Anime like below are my favorite:";
-      description3: "PSYCHO-PASS, Haikyu, JujutsuKaisen, Naruto, One Piece, Attack on Titan and so on.";
-    };
+      "title": "Reading comics and watching anime",
+      "description1": "I'm a big fan of anime and comics. I've read countless comics and anime.",
+      "description2": "Anime like below are my favorite:",
+      "description3": "PSYCHO-PASS, Haikyu, JujutsuKaisen, Naruto, One Piece, Attack on Titan and so on."
+    },
     "going to a restaurant": {
-      title: "Going to a restaurant";
-      description: "I love to eat a food from other countries cuz through the food, I can learn about the culture of the country. It really broadens my horizons.";
-    };
-  };
-  admin: {
-    Admin: "Admin";
-    Applications: "Applications";
-    "Move to the site": "Move to the site";
-    "Move to company site": "Move to company site";
-  };
+      "title": "Going to a restaurant",
+      "description": "I love to eat a food from other countries cuz through the food, I can learn about the culture of the country. It really broadens my horizons."
+    }
+  },
+  "admin": {
+    "Admin": "Admin",
+    "Applications": "Applications",
+    "Move to the site": "Move to the site",
+    "Move to company site": "Move to company site"
+  },
   "admin_add-app": {
-    "Add Application": "Add Application";
-    Title: "Title";
-    Detail: "Detail";
-    imageUrl: "imageUrl";
-    link: "link";
-    "Type of the URL": "Type of the URL";
-    App: "App";
-    Company: "Company";
-    Submit: "Submit";
-  };
+    "Add Application": "Add Application",
+    "Title": "Title",
+    "Detail": "Detail",
+    "imageUrl": "imageUrl",
+    "link": "link",
+    "Type of the URL": "Type of the URL",
+    "App": "App",
+    "Company": "Company",
+    "Submit": "Submit"
+  },
   "admin_apps_app-id_edit": {
-    "Edit Application": "Edit Application";
-    Title: "Title";
-    Detail: "Detail";
-    imageUrl: "imageUrl";
-    link: "link";
-    "Type of the URL": "Type of the URL";
-    App: "App";
-    Company: "Company";
-    Submit: "Submit";
-  };
-  common: {
-    header: {
-      resume: {
-        abbr: "Open Resume";
-        "aria-label": "resume";
-      };
-      contact: {
-        abbr: "Move to contact page";
-        "aria-label": "contact";
-      };
-      theme: {
-        "aria-label": "theme";
-      };
-      menu: {
-        "aria-label": "Menu";
-        "change-theme-color": "Change theme color";
-      };
-    };
+    "Edit Application": "Edit Application",
+    "Title": "Title",
+    "Detail": "Detail",
+    "imageUrl": "imageUrl",
+    "link": "link",
+    "Type of the URL": "Type of the URL",
+    "App": "App",
+    "Company": "Company",
+    "Submit": "Submit"
+  },
+  "common": {
+    "header": {
+      "resume": {
+        "abbr": "Open Resume",
+        "aria-label": "resume"
+      },
+      "contact": {
+        "abbr": "Move to contact page",
+        "aria-label": "contact"
+      },
+      "theme": {
+        "aria-label": "theme"
+      },
+      "menu": {
+        "aria-label": "Menu",
+        "change-theme-color": "Change theme color"
+      }
+    },
     "admin-header": {
-      Logout: "Logout";
-      Home: "Home";
-    };
+      "Logout": "Logout",
+      "Home": "Home"
+    },
     "side-bar": {
-      home: "Home";
-      contact: "Contact";
-      about: "About";
-    };
-  };
-  contact: {
-    Contact: "Contact";
-    "Your email address": "Your email address";
-    Subject: "Subject";
-    Content: "Content";
-    Submit: "Submit";
-    submitted: {
-      title: "Thank you for your message!";
-      content: "Your message has been sent.";
-      "move-to-home": "Move to home";
-    };
-  };
-  index: {
-    Home: "Home";
-    "Hi! I'm": "Hi! I'm";
-    "Koichi Kimura,": "Koichi Kimura,";
-    "a Frontend developer": "a Frontend developer";
-    "Applications I developed at work": "Applications I developed at work";
-    "Move to the site": "Move to the site";
-    "Move to company site": "Move to company site";
-    Fold: "Fold";
-    "Read more": "Read more";
-  };
-  login: {
-    Login: "Login";
-    Email: "example@email.com";
-  };
+      "home": "Home",
+      "contact": "Contact",
+      "about": "About"
+    }
+  },
+  "contact": {
+    "Contact": "Contact",
+    "Your email address": "Your email address",
+    "Subject": "Subject",
+    "Content": "Content",
+    "Submit": "Submit",
+    "submitted": {
+      "title": "Thank you for your message!",
+      "content": "Your message has been sent.",
+      "move-to-home": "Move to home"
+    }
+  },
+  "index": {
+    "Home": "Home",
+    "Hi! I'm": "Hi! I'm",
+    "Koichi Kimura,": "Koichi Kimura,",
+    "a Frontend developer": "a Frontend developer",
+    "Applications I developed at work": "Applications I developed at work",
+    "Move to the site": "Move to the site",
+    "Move to company site": "Move to company site",
+    "Fold": "Fold",
+    "Read more": "Read more"
+  },
+  "login": {
+    "Login": "Login",
+    "Email": "example@email.com"
+  },
   "not-found": {
-    "page-not-found": "Page not found";
-    "move-to-home": "Move to home";
-  };
-  root: {
-    "Something went wrong": "Something went wrong";
-    "Try reload the page": "Try reload the page";
-  };
-  signup: {
-    Signup: "Signup";
-    Email: "example@email.com";
-  };
+    "page-not-found": "Page not found",
+    "move-to-home": "Move to home"
+  },
+  "root": {
+    "Something went wrong": "Something went wrong",
+    "Try reload the page": "Try reload the page"
+  },
+  "signup": {
+    "Signup": "Signup",
+    "Email": "example@email.com"
+  }
 }
 
 export default Resources;
