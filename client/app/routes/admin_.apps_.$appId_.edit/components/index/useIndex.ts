ccf1d9a7301b4154e68acca2e38caf5483b254bd
kupuma-ru21 <tech.kupumaru@gmail.com>
2025-05-18 00:47:41 -0700

    chore: change file name from admin_apps_app-id_edit to admin_apps_app_id_edit

--- client/app/routes/admin_.apps_.$appId_.edit/components/index/useIndex.ts
+++ client/app/routes/admin_.apps_.$appId_.edit/components/index/useIndex.ts
@@ -2,7 +2,7 @@ import {useNavigation} from "@remix-run/react";
 import {useTranslation} from "react-i18next";
 
 export const useIndex = () => {
-  const {t} = useTranslation("admin_apps_app-id_edit");
+  const {t} = useTranslation("admin_apps_app_id_edit");
 
   const {state} = useNavigation();
   const isSubmitting = state === "submitting" || state === "loading";
